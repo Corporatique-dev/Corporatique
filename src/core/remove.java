c139da08e51821623359539b31e92bfe1610748b
wilkirion <axel.chevrier@gmail.com>
2014-12-02 09:08:04 +0100

    Allow developpers to remove a plugin with the command line "-remove"

---
+++ src/core/remove.java
@@ -0,0 +1,56 @@
+package core;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Remove {
+
+	public void remove() {
+	}
+
+	public boolean remove(String toremove) {
+
+		String configPath = "config";
+		Properties properties = new Properties();
+		try {
+			FileInputStream in = new FileInputStream(configPath);
+			properties.load(in);
+			in.close();
+		} catch (IOException e) {
+			System.out.println("Unable to load config file.");
+		}
+
+		String fromconf = properties.getProperty(toremove, "notfound");
+		if (fromconf != "notfound") {
+
+			String plugins = "plugins";
+			String name = (fromconf.split("."))[1];
+			String directorypath = (this.getClass().getResource("Remove.class")
+					.getPath().split(Remove.class.getName()))[0]
+					+ plugins + "/" + name + "/";
+			File directory = new File(directorypath);
+
+			return removeaux(directory);
+		}
+		return false;
+	}
+
+	public boolean removeaux(File directory) {
+		boolean resultat = true;
+		if (directory.exists()) {
+			File[] files = directory.listFiles();
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].isDirectory()) {
+					resultat &= removeaux(files[i]);
+				} else {
+					resultat &= files[i].delete();
+				}
+			}
+		}
+		resultat &= directory.delete();
+		return (resultat);
+
+	}
+}
