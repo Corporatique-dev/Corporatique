1e5e5127d9af0ce0d90a36e856c9ff3cc7d5c811
stardisblue <stardisblue@gmail.com>
2014-12-14 13:20:33 +0100

    Commenting the plugin interface

--- src/plugins/Plugins.java
+++ src/plugins/Plugins.java
@@ -2,22 +2,46 @@ package plugins;
 
 import java.io.File;
 
-@Pluginspecs(name = "Plugin", 
-	version = "1.0.0", 
-	description = "Parent class, every plugin should be extended from it", 
-	author = "Fati CHEN", 
-	dependencies = "none", 
-	extensions = "none")
+@Pluginspecs(
+		name = "Plugin",
+		version = "1.0.0",
+		description = "Interface, every plugin should implement it",
+		author = "Fati CHEN",
+		dependencies = "none",
+		extensions = "none")
 public interface Plugins {
 	File file_in;
 	File file_out;
 
-	public void Load(File file_in);
-	public void processExtraction();
+	/**
+	 * Loading the selected file in the plugin and sets the output file
+	 * (security measures)s
+	 * 
+	 * @param file_in
+	 *            File to load
+	 * @return void
+	 */
+	public void Load(File file_in, File file_out);
+
+	/**
+	 * Process the text extraction to file_out with options if necessary
+	 * 
+	 * @param options
+	 *            eventuals options of the plugin
+	 * @return void
+	 */
+	public void processExtraction(String[] options);
+
+	/*-------------------
+	 * Getters - Setters - toString
+	 */
 	public File getFileIn();
+
 	public File getFileOut();
+
 	void setFileIn(File file_in);
+
 	void setFileOut(File file_out);
-	
+
 	public String toString();
 }
