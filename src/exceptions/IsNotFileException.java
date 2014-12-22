f2ae20b46c498689dbee5e29b774f70b4d2bb3d4
stardisblue <stardisblue@gmail.com>
2014-12-22 19:01:57 +0100

    tu lowercase

---
+++ src/exceptions/IsNotFileException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+/**
+ * Created by stardisblue on 22/12/2014.
+ */
+public class IsNotFileException extends Exception {
+    public IsNotFileException(String message) {
+        super(System.lineSeparator() + "The given path of the plugin " + message + " is not a file");
+    }
+}
---
+++ src/exceptions/PluginDependenciesNotPresentException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+/**
+ * Created by stardisblue on 21/12/2014.
+ */
+public class PluginDependenciesNotPresentException extends Exception {
+    public PluginDependenciesNotPresentException(String message) {
+        super(System.lineSeparator() + "The dependencies of the plugin " + message + " are not present");
+    }
+}
