e3bad806aaab2397eae55e7fff6c899658418aeb
stardisblue <stardisblue@gmail.com>
2015-01-15 22:29:04 +0100

    moved externalized strings to props

--- src/core/Flags.java
+++ src/core/Flags.java
@@ -10,7 +10,7 @@ import java.util.ResourceBundle;
  * @version 1.0.0
  */
 public class Flags {
-    private static final String BUNDLE_NAME = "core.flags";
+    private static final String BUNDLE_NAME = "core.props.flags";
 
     private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
             .getBundle(BUNDLE_NAME);
